475eed4e8f5d1f2ec243563358f392f0185149b8
Gilles <gilles.chek@gmail.com>
2020-11-24 11:06:22 +0100

    Update deploy-lab.sh

--- deploy-lab.sh
+++ deploy-lab.sh
@@ -51,11 +51,8 @@ TF_VAR_secret_key="${TF_VAR_secret_key:-$SECRET}"
 #echo ".....Exporting $TF_VAR_secret_key"
 export AWS_SECRET_ACCESS_KEY=$TF_VAR_secret_key
 
-#echo ""
-
+#----------------------------------
 read  -p $'Press enter to continue (^C to stop)...\n'
-
-
 
 echo -e "\033[1m"   #Bold ON
 echo "===== PHASE 1: Creating SDDC ==========="
@@ -63,8 +60,9 @@ echo -e "\033[0m"   #Bold OFF
 cd ./p1/main
 terraform init
 terraform apply
-
 cd ../../
+#----------------------------------
+
 export TF_VAR_host=$(terraform output -state=./phase1.tfstate proxy_url)
 
 read  -p $'Press enter to continue (^C to stop)...\n'
@@ -76,26 +74,32 @@ echo "===== PHASE 2: Networking and Security ==========="
 echo -e "\033[0m"   #Bold OFF
 
 terraform apply
+cd ../..
+#----------------------------------
 
 read  -p $'Press enter to continue (^C to stop)...\n'
 echo -e "\033[1m"   #Bold ON
 echo "===== PHASE 3: Create Content Lib and deploy VM  ==========="
 echo -e "\033[0m"   #Bold OFF
-
-cd ../..
 cd ./p3/main
 terraform  init
 terraform apply
 cd ../..
+
+#----------------------------------
 
 read  -p $'Press enter to continue (^C to stop)...\n'
 echo -e "\033[1m"   #Bold ON
 echo "===== PHASE 4: Egress VPC ==========="
 echo -e "\033[0m"   #Bold OFF
 
-read -p "Enter your SDDC VPN IP: " TF_VAR_SDDC_VPN_publicIP
+read -p "Enter your SDDC VPN Public IP: " TF_VAR_SDDC_VPN_publicIP
 echo ".....Exporting $TF_VAR_SDDC_VPN_publicIP"
 export TF_VAR_SDDC_VPN_publicIP=$TF_VAR_SDDC_VPN_publicIP
 echo ""
+cd ./p4/main
+terraform  init
+terraform apply
+cd ../../
 
-
+#----------------------------------
